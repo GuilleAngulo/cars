082419207552c73491bab7a3a9b2aecf254b56f5
Guillermo Angulo <anyermo@gmail.com>
2020-08-08 20:46:14 -0300

    eslint conflicts solved

---
+++ database/migrations/01_create_faq.ts
@@ -0,0 +1,14 @@
+import Knex from "knex";
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable("faq", (table) => {
+    table.increments("id").primary();
+    table.string("question").notNullable();
+    table.string("answer").notNullable();
+    table.date("createDate").notNullable();
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable("faq");
+}
