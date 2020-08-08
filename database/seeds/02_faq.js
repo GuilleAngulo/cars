d6574ee977becb8e9af699c99deb209616dcef3f
Guillermo Angulo <anyermo@gmail.com>
2020-08-08 15:31:57 -0300

    feat: faq seed fixed

--- database/seeds/02_faq.js
+++ database/seeds/02_faq.js
@@ -7,35 +7,35 @@ module.exports = {
         question: "How to be safe buying online?",
         answer:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo",
-        date: new Date(),
+        createDate: new Date(),
       },
       {
         id: 2,
         question: "Do I have any assurance on my new car?",
         answer:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo",
-        date: new Date(),
+        createDate: new Date(),
       },
       {
         id: 3,
         question: "How many kilometers a normal car can have?",
         answer:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo",
-        date: new Date(),
+        createDate: new Date(),
       },
       {
         id: 4,
         question: "What is the best month to buy a car?",
         answer:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo",
-        date: new Date(),
+        createDate: new Date(),
       },
       {
         id: 5,
         question: "How to know the car history?",
         answer:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo",
-        date: new Date(),
+        createDate: new Date(),
       },
     ]);
   },
