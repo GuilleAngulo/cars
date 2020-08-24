6d0c282ec8d860894a6ed116d9614eb8f2a92fc6
Guillermo Angulo <anyermo@gmail.com>
2020-08-24 17:48:07 -0300

    skeleton improved

--- components/CarLoadingList/index.tsx
+++ components/CarLoadingList/index.tsx
@@ -14,21 +14,21 @@ export default function CarLoadingList({ count }: CarLoadingListProps) {
 
 export function CarItemLoading() {
     return (
-        <div className="border border-gray-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-            <div className="animate-pulse h-full space-y-2">
+        <div className="border border-gray-300 shadow-lg rounded-md max-w-sm w-full">
+            <div className="animate-pulse h-full">
                 <div className="rounded relative pb-2/3">
                     <div className="absolute rounded bg-gray-400 w-full h-full"></div>
                 </div>
-                <div className="space-y-2 py-2">
-                    <div className="h-5 bg-gray-400 rounded w-3/4"></div>
-                    <div className="h-5 bg-gray-400 rounded w-1/4"></div>
-                    <div className="space-y-1">
-                        <div className="h-5 bg-gray-400 rounded"></div>
-                        <div className="h-5 bg-gray-400 rounded"></div>
-                        <div className="h-5 bg-gray-400 rounded"></div>
-                        <div className="h-5 bg-gray-400 rounded"></div>
-                        <div className="h-5 bg-gray-400 rounded"></div>
-                        <div className="h-5 bg-gray-400 rounded"></div>
+                <div className="px-6 py-4">
+                    <div className="h-6 bg-gray-400 rounded w-3/4 mb-4"></div>
+                    <div className="h-5 bg-gray-400 rounded w-1/4 mb-4"></div>
+                    <div className="space-y-2">
+                        <div className="h-4 bg-gray-400 rounded"></div>
+                        <div className="h-4 bg-gray-400 rounded"></div>
+                        <div className="h-4 bg-gray-400 rounded"></div>
+                        <div className="h-4 bg-gray-400 rounded"></div>
+                        <div className="h-4 bg-gray-400 rounded"></div>
+                        <div className="h-4 bg-gray-400 rounded"></div>
                     </div>
                 </div>
             </div>
